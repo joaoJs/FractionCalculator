0d1345063c105584e1e8f38931ab54a06725aa76
Joao Campos <j.campos4g@gmail.com>
2017-10-30 00:54:29 -0400

    added subtract()

--- src/Fraction.java
+++ src/Fraction.java
@@ -3,9 +3,9 @@ public class Fraction {
     private int denominator;
 
     public static void main(String[] args) {
-        Fraction first = new Fraction(1,3);
-        Fraction second = new Fraction(1,2);
-        first.add(second);
+        Fraction first = new Fraction(4,1);
+        Fraction second = new Fraction(3,2);
+        first.subtract(second);
     }
 
     public Fraction(int num, int den) {
@@ -69,9 +69,31 @@ public class Fraction {
         return addedFraction;
     }
 
-//    public double subtract(Fraction other) {
-//
-//    }
+    public Fraction subtract(Fraction other) {
+        int num = 0;
+        int den = 1;
+        if (this.denominator == other.denominator) {
+            num = this.numerator + other.numerator;
+            den = this.denominator;
+        } else if (this.denominator < other.denominator) {
+            int cons = this.denominator;
+            while (this.denominator % other.denominator != 0) {
+                this.denominator += cons;
+            }
+            num = (this.numerator * this.denominator / cons) - (other.numerator * this.denominator / other.denominator);
+            den = this.denominator;
+        } else if (this.denominator > other.denominator) {
+            int cons = other.denominator;
+            while (other.denominator % this.denominator != 0) {
+                other.denominator += cons;
+            }
+            num = (this.numerator * other.denominator / this.denominator) - (other.numerator * other.denominator / cons);
+            den = other.denominator;
+        }
+        Fraction subtractedFraction = new Fraction(num, den);
+        System.out.println("Here --> " + subtractedFraction.toString());
+        return subtractedFraction;
+    }
 //
 //    public double multiply(Fraction other) {
 //

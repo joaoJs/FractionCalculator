86b781ab7eb32adb13132ba25ef391588c198029
Joao Campos <j.campos4g@gmail.com>
2017-10-29 23:38:02 -0400

    implemented add()

--- src/Fraction.java
+++ src/Fraction.java
@@ -2,6 +2,12 @@ public class Fraction {
     private int numerator;
     private int denominator;
 
+    public static void main(String[] args) {
+        Fraction first = new Fraction(1,3);
+        Fraction second = new Fraction(1,2);
+        first.add(second);
+    }
+
     public Fraction(int num, int den) {
         if (den == 0) {
             throw new IllegalArgumentException();
@@ -20,5 +26,59 @@ public class Fraction {
     public Fraction() {
         this(0, 1);
     }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public double toDouble() {
+        return (double)(numerator / denominator);
+    }
+
+    public Fraction add(Fraction other) {
+        int num = 0;
+        int den = 1;
+        if (this.denominator == other.denominator) {
+            num = this.numerator + other.numerator;
+            den = this.denominator;
+        } else if (this.denominator < other.denominator) {
+            int cons = this.denominator;
+            while (this.denominator % other.denominator != 0) {
+                this.denominator += cons;
+            }
+            num = (this.numerator * this.denominator / cons) + (other.numerator * this.denominator / other.denominator);
+            den = this.denominator;
+        } else if (this.denominator > other.denominator) {
+            int cons = other.denominator;
+            while (other.denominator % this.denominator != 0) {
+                other.denominator += cons;
+            }
+            num = (this.numerator * other.denominator / this.denominator) + (other.numerator * other.denominator / cons);
+            den = other.denominator;
+        }
+        Fraction addedFraction = new Fraction(num, den);
+        System.out.println("Here --> " + addedFraction.toString());
+        return addedFraction;
+    }
+
+//    public double subtract(Fraction other) {
+//
+//    }
+//
+//    public double multiply(Fraction other) {
+//
+//    }
+//
+//    public double divide(Fraction other) {
+//
+//    }
 
 }

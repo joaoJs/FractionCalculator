8eb8189c20d10fb684ef1fa8c3950fae75a7d567
Joao Campos <j.campos4g@gmail.com>
2017-10-30 14:17:43 -0400

    added validFraction()

--- src/FractionCalculator.java
+++ src/FractionCalculator.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import  java.util.Arrays;
 public class FractionCalculator {
 
     public static void main(String[] args) {
-        String op = getOperation();
-        System.out.println(op);
+        //String op = getOperation();
+        //System.out.println(op);
+        boolean is = validFraction("67/5");
+        System.out.println(is);
     }
 
     public static String getOperation() {
@@ -16,4 +19,40 @@ public class FractionCalculator {
         }
         return operation;
     }
-}
+
+    public static boolean validFraction(String input) {
+        if (input.indexOf("-") > 0) {
+            return false;
+        } else if (input.indexOf("/") == -1) {
+            return false;
+        } else {
+            String[] arr = input.split("[/]");
+            System.out.println(Arrays.toString(arr));
+            if (arr.length > 2) {
+                return false;
+            } else {
+                if (isNumber(arr[0]) && isNumber(arr[1])) {
+                    System.out.println(Integer.parseInt(arr[0]));
+                    System.out.println(Integer.parseInt(arr[1]));
+                    if (Integer.parseInt(arr[1]) == 0) {
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+
+        }
+    }
+
+    private static boolean  isNumber(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if ("0123456789-".indexOf(input.charAt(i)) == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+ }
